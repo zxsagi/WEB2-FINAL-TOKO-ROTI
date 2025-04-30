8de7ae06b24ab0ae4f25638183e0ed0c54b863c1
Sagi Setiono <2382016@unai.edu>
2025-04-30 22:54:16 +0700

    Update AxiosInstance.ts

--- src/utils/AxiosInstance.ts
+++ src/utils/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL 
+    baseURL: import.meta.env.VITE_API_BASE, 
 });
 
 export default AxiosInstance;
